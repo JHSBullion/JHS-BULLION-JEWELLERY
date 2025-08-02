ef79d007db588f8e6abf3968ead0d0a2ff5d0f14
JHSBullion <jhsbullionjewellery@gmail.com>
2025-08-03 00:18:22 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,47 +1,46 @@
 window.goldPrices = {
-  "375": {
-    "current": 347,
-    "diff": 147,
-    "history": [
-      100,
-      200,
-      347
-    ]
-  },
-  "750": {
-    "current": 406,
-    "diff": 106,
-    "history": [
-      200,
-      300,
-      406
-    ]
-  },
   "835": {
-    "current": 442,
-    "diff": 42,
+    "current": 347,
+    "diff": -95,
     "history": [
       378,
       400,
-      442
+      442,
+      347
     ]
   },
   "916": {
-    "current": 446,
-    "diff": -10,
+    "current": 406,
+    "diff": -40,
     "history": [
       455,
       456,
-      446
+      446,
+      406
     ]
   },
   "999": {
-    "current": 452,
-    "diff": -15,
+    "current": 442,
+    "diff": -10,
     "history": [
       467,
       467,
+      452,
+      442
+    ]
+  },
+  "999.9P": {
+    "current": 452,
+    "diff": 0,
+    "history": [
       452
+    ]
+  },
+  "999.9": {
+    "current": 446,
+    "diff": 0,
+    "history": [
+      446
     ]
   }
 };
