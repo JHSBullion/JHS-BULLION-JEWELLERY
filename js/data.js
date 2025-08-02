d1a11ccc3684e4e8f425327e32955b13cc3caa90
JHSBullion <jhsbullionjewellery@gmail.com>
2025-08-02 21:21:04 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,7 @@
+window.goldPrices = {
+  "999": { "current": 999, "diff": -213 },
+  "916": { "current": 999, "diff": -213 },
+  "835": { "current": 999, "diff": -213 },
+  "750": { "current": 999, "diff": -213 },
+  "375": { "current": 999, "diff": -222 }
+};
