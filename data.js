02eb83eeddc76ab90e70077eb1f1aed79f894b07
JHSBullion <jhsbullionjewellery@gmail.com>
2025-08-02 22:09:13 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+window.goldPrices = {
+  "375": {
+    "current": 100,
+    "diff": 0,
+    "history": [
+      100
+    ]
+  },
+  "750": {
+    "current": 200,
+    "diff": 0,
+    "history": [
+      200
+    ]
+  },
+  "835": {
+    "current": 378,
+    "diff": 0,
+    "history": [
+      378
+    ]
+  },
+  "916": {
+    "current": 455,
+    "diff": 0,
+    "history": [
+      455
+    ]
+  },
+  "999": {
+    "current": 467,
+    "diff": 0,
+    "history": [
+      467
+    ]
+  }
+};
