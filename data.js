cb15a80de9ba8099c99cc5e70f6b16427ad647b5
JHSBullion <jhsbullionjewellery@gmail.com>
2025-08-02 20:33:05 +0800

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-window.goldPrices = {
-  "999": { "current": 999, "diff": -213 },
-  "916": { "current": 999, "diff": -213 },
-  "835": { "current": 999, "diff": -213 },
-  "750": { "current": 999, "diff": -213 },
-  "375": { "current": 999, "diff": -222 }
-};
