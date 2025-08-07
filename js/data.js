dea1c5812c97f53ac0640b491ce6860d94caa1f0
JHSBullion <jhsbullionjewellery@gmail.com>
2025-08-07 09:48:58 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,51 +1,71 @@
 window.goldPrices = {
   "835": {
-    "current": 345,
-    "diff": 0,
+    "current": 344,
+    "diff": -1,
     "history": [
       {
         "date": "2025-08-06",
         "value": 345
+      },
+      {
+        "date": "2025-08-07",
+        "value": 344
       }
     ]
   },
   "916": {
-    "current": 404,
-    "diff": 0,
+    "current": 403,
+    "diff": -1,
     "history": [
       {
         "date": "2025-08-06",
         "value": 404
+      },
+      {
+        "date": "2025-08-07",
+        "value": 403
       }
     ]
   },
   "999": {
-    "current": 439,
-    "diff": 0,
+    "current": 438,
+    "diff": -1,
     "history": [
       {
         "date": "2025-08-06",
         "value": 439
+      },
+      {
+        "date": "2025-08-07",
+        "value": 438
       }
     ]
   },
   "999.9P": {
-    "current": 449,
-    "diff": 0,
+    "current": 448,
+    "diff": -1,
     "history": [
       {
         "date": "2025-08-06",
         "value": 449
+      },
+      {
+        "date": "2025-08-07",
+        "value": 448
       }
     ]
   },
   "999.9": {
-    "current": 443,
-    "diff": 0,
+    "current": 442,
+    "diff": -1,
     "history": [
       {
         "date": "2025-08-06",
         "value": 443
+      },
+      {
+        "date": "2025-08-07",
+        "value": 442
       }
     ]
   }
